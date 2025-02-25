af26aa3b895373f84877c4f9d0116169ad6f08bc
samadsayyed <samadsayyed812@gmail.com>
2025-02-25 16:43:13 +0530

    made it stylish

--- routes/auth.php
+++ routes/auth.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
+    // test commit
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
